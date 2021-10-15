b299548e18676b9a41485fa97bf3a987d7c09bfd
unknown <fourati.oussama9@gmail.com>
2021-10-15 17:56:02 +0200

    layout is added

---
+++ src/layout/partials/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Navbar, Nav} from "react-bootstrap"
+import Logo from '../../assets/img/logo.png'
+
+export const Header = () => {
+    return (
+        <Navbar
+        collapseOnSelect bg="info" 
+        variant="dark" expand="md"
+        >
+            <Navbar.Brand>
+                <img src={Logo} alt="logo" width="50px" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                    <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                    <Nav.Link href="/dashboard">Tickets</Nav.Link>
+                    <Nav.Link href="/dashboard">Logout</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
