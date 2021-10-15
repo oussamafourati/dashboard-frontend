60b51480e9ba99bb37e9653c52e70958b0399442
unknown <fourati.oussama9@gmail.com>
2021-10-15 21:17:53 +0200

    dashboard ui added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/DashboardPage';
 import { Entry } from './pages/entry/Entry.page';
 function App() {
   return (
     <div className="App">
      {/*<Entry/>*/}
      <DefaultLayout>
-       
+       <Dashboard/>
      </DefaultLayout>
     </div>
   );
